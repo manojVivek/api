3cf53ee763fb6b1245aafda3ecfbdda9b9806fe6
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-03-24 15:49:22 +0530

    Removed redundant field and error handling

--- raw_data-to-state_district_wise_data.js
+++ raw_data-to-state_district_wise_data.js
@@ -2,43 +2,47 @@ const fs = require('fs');
 const rawData = require('./raw_data');
 
 console.log('Starting district wise data processing');
-const StateDistrictWiseData = rawData.raw_data.reduce((acc, row) => {
-  let stateName = row.detectedstate;
-    if(!stateName) {
-      stateName = 'Unknown';
+try {
+  const StateDistrictWiseData = rawData.raw_data.reduce((acc, row) => {
+    let stateName = row.detectedstate;
+      if(!stateName) {
+        stateName = 'Unknown';
+      }
+    if(!acc[stateName]) {
+      acc[stateName] = {districtData: {}};
     }
-  if(!acc[stateName]) {
-    acc[stateName] = {state: stateName, districtData: {}};
-  }
-  let districtName = row.detecteddistrict;
-    if(!districtName) {
-      districtName = 'Unknown';
+    let districtName = row.detecteddistrict;
+      if(!districtName) {
+        districtName = 'Unknown';
+      }
+    if(!acc[stateName].districtData[districtName]) {
+      
+      acc[stateName].districtData[districtName] = {
+        active: 0,
+        confirmed: 0,
+        deaths: 0,
+        lastupdatedtime: "",
+        recovered: 0,
+      };
     }
-  if(!acc[stateName].districtData[districtName]) {
-    
-    acc[stateName].districtData[districtName] = {
-			active: 0,
-			confirmed: 0,
-			deaths: 0,
-			lastupdatedtime: "",
-			recovered: 0,
-		};
-  }
-  const currentDistrict = acc[stateName].districtData[districtName];
-
-  currentDistrict.confirmed++;
-  if(row.currentstatus === 'Hospitalized') {
-    currentDistrict.active++;
-  } else if(row.currentstatus === 'Deceased') {
-    currentDistrict.deaths++;
-  } else if(row.currentstatus === 'Recovered') {
-    currentDistrict.recovered++;
-  }
-
-  return acc;
-
-}, {});
-
-fs.writeFileSync('state_district_wise_raw.json', JSON.stringify(StateDistrictWiseData, null, 2));
-console.log('Starting district wise data processing ...done');
+    const currentDistrict = acc[stateName].districtData[districtName];
+  
+    currentDistrict.confirmed++;
+    if(row.currentstatus === 'Hospitalized') {
+      currentDistrict.active++;
+    } else if(row.currentstatus === 'Deceased') {
+      currentDistrict.deaths++;
+    } else if(row.currentstatus === 'Recovered') {
+      currentDistrict.recovered++;
+    }
+  
+    return acc;
+  
+  }, {});
+  
+  fs.writeFileSync('state_district_wise_raw.json', JSON.stringify(StateDistrictWiseData, null, 2));
+  console.log('Starting district wise data processing ...done');
+} catch(err) {
+  console.log('Error processing district wise data', err);
+}
 
